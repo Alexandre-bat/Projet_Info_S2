39455a0a777225273f318bb0bc215b19e2e128b6
Achille <achille.therin@etu.cyu.fr>
2026-03-24 18:31:22 +0100

    Adaptation carte
    carte adaptative desormais

--- Menu.php
+++ Menu.php
@@ -1,4 +1,8 @@
 <?php include("start.php"); ?>
+<?php
+$json = file_get_contents("carte.json");
+$produits = json_decode($json, true);
+?>
 <!DOCTYPE html>
 <html>
 
@@ -62,90 +66,24 @@
                 <h1>Entrées</h1>
             </div>
             <div class="controlBox">
+                <?php foreach($produits as $p){
+                    if($p['categorie'] == "entree"){
+                ?>
                 <div class="box">
-                    <img src="Img/Imagesmenu/gyoza.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
+                    <img src="Img/Imagesmenu/<?php echo $p['img']; ?>" alt="<?php echo $p['nom']; ?>" class="imgBox">
                     <div class="contenuBox">
-                        <h2>Bol de Gyoza</h2>
-                        <p>Notre bol de gyoza fait maison </p>
-                        <p>Porc haché, chou chinois, ail, gingembre, sauce soja, huile de sésame, pâte à gyoza</p>
+                        <h2><?php echo $p['nom']; ?></h2>
+                        <p><?php echo $p['description']; ?></p>
+                        <p><?php echo $p['ingredients']; ?></p>
                     </div>
                     <div class="basBox">
-                        <span id="prix">Prix : 9€</span>
-                        <a href="panier.php?produit=gyoza">
+                        <span id="prix">Prix : <?php echo $p['prix']; ?>€</span>
+                        <a href="panier.php?produit=<?php echo $p['id']; ?>">
                             <button class="bouttonclassique">Commander</button>
                         </a>
                     </div>
                 </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/rolls.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Rona-roll-do</h2>
-                        <p>Nos rolls signatures</p>
-                        <p>Riz vinaigré, algue nori, saumon, thon, avocat, concombre, fromage frais</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 10€</span>
-                        <a href="panier.php?produit=rolls">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/sashimi.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Bol de Siuuushimi</h2>
-                        <p>Notre bol de sashimi préparer à la main</p>
-                        <p>Saumon cru, thon cru, daurade, sauce soja, wasabi, gingembre mariné</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 12€</span>
-                        <a href="panier.php?produit=sashimi">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/petitsSushi.jpeg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Assortiment de delicieux Siuuushi</h2>
-                        <p>Un assortiment de nos meilleurs sushis préparés par le chef en personne</p>
-                        <p>Riz vinaigré, saumon, thon, crevette, avocat, algue nori, fromage frais, concombre</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 10€</span>
-                        <a href="panier.php?produit=petitsSushi">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/maki.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Notre Ball (bol) de Maki et sa sauce</h2>
-                        <p>Notre bol de maki à déguster en famille ou entre amis</p>
-                        <p>Riz vinaigré, algue nori, saumon, thon, avocat, concombre, fromage frais</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 9€</span>
-                        <a href="panier.php?produit=maki">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/brochettes.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Panenka (brochette) de boeuf</h2>
-                        <p>Un ensemble de brochettes de boeuf cuites à la perfection</p>
-                        <p>Boeuf, fromage, sauce teriyaki, sucre, sauce soja, mirin</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 11€</span>
-                        <a href="panier.php?produit=brochettes">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
+                <?php }} ?>
             </div>
         </div>
         <div class="blocMenu">
@@ -153,91 +91,24 @@
                 <h3>Plats</h3>
             </div>
             <div class="controlBox">
+                <?php foreach($produits as $p){
+                    if($p['categorie'] == "plat"){
+                ?>
                 <div class="box">
-                    <img src="Img/Imagesmenu/SR.jpeg" alt="Image du Supreme Ronaldo" class="imgBox">
+                    <img src="Img/Imagesmenu/<?php echo $p['img']; ?>" alt="<?php echo $p['nom']; ?>" class="imgBox">
                     <div class="contenuBox">
-                        <h2>Supreme ronaldo</h2>
-                        <p>Le plat ultime de ronaldo à partager</p>
-                        <p>Riz vinaigré, saumon, thon, avocat, concombre, algue nori, fromage frais, crevette</p>
+                        <h2><?php echo $p['nom']; ?></h2>
+                        <p><?php echo $p['description']; ?></p>
+                        <p><?php echo $p['ingredients']; ?></p>
                     </div>
                     <div class="basBox">
-                        <span id="prix">Prix : 67€</span>
-                        <a href="panier.php?produit=supremeRonaldo">
+                        <span id="prix">Prix : <?php echo $p['prix']; ?>€</span>
+                        <a href="panier.php?produit=<?php echo $p['id']; ?>">
                             <button class="bouttonclassique">Commander</button>
                         </a>
                     </div>
                 </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/chirashi.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Chirashi</h2>
-                        <p>Notre plat de chirashi fait maison</p>
-                        <p>Riz vinaigré, saumon, thon, concombre, graines de sésame, sauce soja</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 17€</span>
-                        <a href="panier.php?produit=chirashi">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/chirashiAvocat.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Chirashi avocat</h2>
-                        <p>Notre plat de chirashi revisité avec des avocats</p>
-                        <p>Riz vinaigré, saumon, avocat, concombre, graines de sésame, sauce soja</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 18€</span>
-                        <a href="panier.php?produit=chirashiAvocat">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/grosSushi.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Assiette de sushi</h2>
-                        <p>Notre belle assiette de sushi complète avec plusieurs variétés de poissons</p>
-                        <p>Riz vinaigré, saumon, thon, crevette, avocat, algue nori, fromage frais, concombre</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 21€</span>
-                        <a href="panier.php?produit=grosSushi">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/pokeBowl.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Poke GOAL</h2>
-                        <p>Notre poke (bowl) ball jouée (presque) officiellement par Ronaldo</p>
-                        <p>Riz, saumon cru, avocat, concombre, carotte, graines de sésame, sauce soja, mangue</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 15€</span>
-                        <a href="panier.php?produit=pokeBowl">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/bobun.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Ballbun</h2>
-                        <p>Notre Bobun au poulet et au nems végétariens</p>
-                        <p>Vermicelles de riz, boeuf sauté, nems, carotte, concombre, salade, menthe, cacahuètes, sauce
-                            nuoc-mâm</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 12€</span>
-                        <a href="panier.php?produit=bobun">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
+                <?php }} ?>
             </div>
         </div>
         <div class="blocMenu">
@@ -245,76 +116,24 @@
                 <h3>Desserts</h3>
             </div>
             <div class="controlBox">
+                <?php foreach($produits as $p){
+                    if($p['categorie'] == "dessert"){
+                ?>
                 <div class="box">
-                    <img src="Img/Imagesmenu/mochinutella.png" alt="Image du Supreme Ronaldo" class="imgBox">
+                    <img src="Img/Imagesmenu/<?php echo $p['img']; ?>" alt="<?php echo $p['nom']; ?>" class="imgBox">
                     <div class="contenuBox">
-                        <h2>mochi au nutella</h2>
-                        <p>Notre mocchi nutella au coeur glacé</p>
-                        <p>Pâte de riz gluant, Nutella, sucre, eau</p>
+                        <h2><?php echo $p['nom']; ?></h2>
+                        <p><?php echo $p['description']; ?></p>
+                        <p><?php echo $p['ingredients']; ?></p>
                     </div>
                     <div class="basBox">
-                        <span id="prix">Prix : 5€</span>
-                        <a href="panier.php?produit=mochiNutella">
+                        <span id="prix">Prix : <?php echo $p['prix']; ?>€</span>
+                        <a href="panier.php?produit=<?php echo $p['id']; ?>">
                             <button class="bouttonclassique">Commander</button>
                         </a>
                     </div>
                 </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/mochisCoco.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Mochi coco</h2>
-                        <p>Notre mochi coco au coeur glacé</p>
-                        <p>Pâte de riz gluant, noix de coco, sucre, lait de coco</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 5€</span>
-                        <a href="panier.php?produit=mochiCoco">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/cheesecake.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Cheesecake japonais</h2>
-                        <p>Notre cheesecake japonais moelleux à souhait</p>
-                        <p>Fromage frais, oeufs, sucre, lait, farine, beurre, vanille</P>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 7€</span>
-                        <a href="panier.php?produit=cheesecake">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/perlesCoco.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Perles coco</h2>
-                        <p>Nos perles coco faites mains</p>
-                        <p>Farine de riz gluant, noix de coco râpée, sucre, haricot mungo, lait de coco</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 5€</span>
-                        <a href="panier.php?produit=perlesCoco">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="box">
-                    <img src="Img/Imagesmenu/sake.jpg" alt="Image du Supreme Ronaldo" class="imgBox">
-                    <div class="contenuBox">
-                        <h2>Verre de sake</h2>
-                        <p>Un verre de sake traditionnel</p>
-                        <p>Riz fermenté, eau, koji, levure</p>
-                    </div>
-                    <div class="basBox">
-                        <span id="prix">Prix : 6€</span>
-                        <a href="panier.php?produit=sake">
-                            <button class="bouttonclassique">Commander</button>
-                        </a>
-                    </div>
-                </div>
+                <?php }} ?>
             </div>
         </div>
     </main>
