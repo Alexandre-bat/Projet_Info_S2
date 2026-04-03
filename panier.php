12ba744a8167449fbfcc15925c94864c365e747d
Achille <achille.therin@etu.cyu.fr>
2026-04-03 19:14:54 +0200

    footer partout réuni

--- panier.php
+++ panier.php
@@ -147,29 +147,7 @@ function getProduit($produits, $id){
 </main>
                     
     <footer>
-        <div class="f-container">
-            <div class="f-section">
-                <h4>À PROPOS</h4>
-                <p>SIUUSHI - Restaurant traditionnel depuis 1967</p>
-                <p>Une expérience culinaire traditionnel</p>
-            </div>
-
-            <div class="f-section">
-                <h4>CONTACT</h4>
-                <p>📍 Parc des princes</p>
-                <p>📞 +33 1 23 45 67 89</p>
-            </div>
-
-            <div class="f-section">
-                <h4>HORAIRES</h4>
-                <p>Lun - Sam: 11h00 - 14h00 / 18h00 - 23h00</p>
-                <p> Livraison disponible</p>
-            </div>
-        </div>
-
-        <div class="f-bottom">
-            <p>&copy; 2026 SIUUSHI - Tous droits réservés | Mentions légales | Politique de confidentialité</p>
-        </div>
+        <?php include("Utilitaire/footer.php"); ?>
     </footer>
 </body>
 
