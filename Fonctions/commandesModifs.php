6b16cbbc9867bc15779e8035b0247cac2ca3415f
Achille <achille.therin@etu.cyu.fr>
2026-04-05 21:18:10 +0200

    changement nom (obliger de faire à la main un par un car bug)

--- Fonctions/commandesModifs.php
+++ Fonctions/commandesModifs.php
@@ -9,7 +9,7 @@
     //verification de quelle fonction appelée
 
     function attenteToPrepa($idCommande){
-        $contenu = file_get_contents(__DIR__ ."/../.json/commandes.json");
+        $contenu = file_get_contents(".json/commandes.json");
         $data = json_decode($contenu, true);
         if (!is_array($data)) {
             $data = [];
@@ -19,14 +19,14 @@
                 $commande["Statut"] = "En preparation";
             }
         }
-        file_put_contents(__DIR__ ."/../.json/commandes.json", json_encode($data, JSON_PRETTY_PRINT));
+        file_put_contents(".json/commandes.json", json_encode($data, JSON_PRETTY_PRINT));
         header("Location:  ../Commandes.php"); 
         exit();
     }
     // Si cette fonction est appelée le statut de la commande devient "En preparation"
 
     function prepaToLivraison($idCommande){
-        $contenu = file_get_contents(__DIR__ ."/../.json/commandes.json");
+        $contenu = file_get_contents(".json/commandes.json");
         $data = json_decode($contenu, true);
         if (!is_array($data)) {
             $data = [];
@@ -36,7 +36,7 @@
                 $commande["Statut"] = "En livraison";
             }
         }
-        file_put_contents(__DIR__ ."/../.json/commandes.json", json_encode($data, JSON_PRETTY_PRINT));
+        file_put_contents(".json/commandes.json", json_encode($data, JSON_PRETTY_PRINT));
         header("Location:  ../Commandes.php"); 
         exit();
     }
