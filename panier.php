8a34b77ebdf86c1dfc892124664dc442308f1ef1
Achille <achille.therin@etu.cyu.fr>
2026-04-05 20:56:42 +0200

    push encore casse

--- panier.php
+++ panier.php
@@ -84,7 +84,7 @@
                         <div class="blocGauchePanier">
                             <h2>Total : <?php echo $total; ?>€</h2>
                             <?php  $_SESSION['prix'] = $total; ?>
-                            <form action="panier.php" method="post">
+                            <form action="Panier.php" method="post">
                                 <button class="bouttonclassique" name="vider">Vider le panier</button>
                             </form>
                         </div>
