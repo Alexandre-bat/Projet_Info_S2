052534d769cbbc851e869c49e115024d4fbde58b
YarthiasY <cordero.mathias.2006@gmail.com>
2026-05-11 16:17:36 +0200

    Light/Dark theme bouton

--- Utilitaire/nav.php
+++ Utilitaire/nav.php
@@ -1,3 +1,6 @@
+<link id="theme-style" rel="stylesheet" href="Light-Style.css">
+
+
 <div class="navbar">
     <div class="nav1">
         <a href="Accueil.php" class="menu">
@@ -7,6 +10,7 @@
     </div>
 
     <div class="nav2">
+
         <?php
             if($role === "admin") {
                 echo '<a href="Admin.php">Admin</a>';
@@ -19,28 +23,39 @@
             else if($role === "restaurateur"){
                 echo '<a href="Commandes.php">Commandes</a>';
             }
-            //affichage different selon le type de compte: rien pour client, livraison pour livreur, commandes pour restaurateur, tout pour admin
         ?>
+
         <a href="Carte.php">Carte</a>
+
         <?php 
             if(isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
+
                 echo '<a href="Profil.php">' . $_SESSION['nom'] . ' ' . $_SESSION['prenom'] . ' '. '<img src="Img/profil.png" alt="Logo" class="profil_nav">' .'</a>';
+
                 echo '<a href="Accueil.php?deco=1">Déconnexion</a>';
+
                 $nbrsession = 0;
+
                 if(isset($_SESSION['panier'])){
                     $nbrsession = count($_SESSION['panier']);
         ?>
+
         <a href="Panier.php">
-            <img src="Img/panier.png" alt="Panier" id="logoPanier"><?php echo $nbrsession;?>
+            <img src="Img/panier.png" alt="Panier" id="logoPanier">
+            <?php echo $nbrsession;?>
         </a>
+
         <?php
                 }
             } else {
+
                 echo '<a href="Connexion.php">Connexion</a>';
                 echo '<a href="Inscription.php">Inscription</a>';
             }
-            //affiche en fonction de si un compte est connecté ou non : son profil, son panier et une option de deconnexion ou des boutons de connexion de d'inscription
         ?>
+
+        <button id="theme-btn" class="theme"></button>
+
     </div>
 </div>
 
@@ -49,3 +64,32 @@
     <source src="Img/fond.mp4" type="video/mp4">
     Votre navigateur ne supporte pas la vidéo.
 </video>
+
+<script>
+function setTheme(theme) {
+    document.getElementById("theme-style").href = theme;
+    localStorage.setItem("theme", theme);
+    updateThemeBtn(theme);
+}
+
+function updateThemeBtn(theme) {
+    const btn = document.getElementById("theme-btn");
+
+    if (theme === "Dark_Style.css") {
+        btn.innerHTML = "Light";
+        btn.onclick = () => setTheme("Light-Style.css");
+    } else {
+        btn.innerHTML = "Dark";
+        btn.onclick = () => setTheme("Dark_Style.css");
+    }
+}
+
+window.onload = function () {
+
+    const savedTheme = localStorage.getItem("theme") || "Light-Style.css";
+
+    document.getElementById("theme-style").href = savedTheme;
+
+    updateThemeBtn(savedTheme);
+}
+</script>
