37ed0be9f61e97e931c6db5b8e7adfef9839d227
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2026-06-01 14:38:48 +0200

    Revert "Merge branch 'main' of https://github.com/Alexandre-bat/Projet_Info_S2"
    This reverts commit ebebf5253f181e163ef2b2f4d90def0ced6eb18b, reversing
    changes made to 622eb1141f023b3cfca7b9dcca2eff5052117665.

--- routeur.php
+++ routeur.php
@@ -1,5 +1,4 @@
 <?php
-    //securite pour ne pas pouvoir trouver les jsons par url
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     if (str_starts_with($uri, '/json/')) {
         http_response_code(403);
